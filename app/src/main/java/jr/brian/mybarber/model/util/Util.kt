b4b7a2ec7dd86710e63a1df81e26924d16ad1306
Brian Walker Jr <thaballa79@gmail.com>
2022-07-25 20:43:56 -0500

    fixes sign in/ sign up fragments

---
+++ app/src/main/java/jr/brian/mybarber/model/util/Util.kt
@@ -0,0 +1,25 @@
+package jr.brian.mybarber.model.util
+
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.snackbar.Snackbar
+
+fun AppCompatActivity.replaceFragment(containerId: Int, fragment: Fragment) {
+    supportFragmentManager.inTransaction { replace(containerId, fragment) }
+}
+
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().func().commit()
+}
+
+fun showSnackbar(str: String, view: View, id: Int) {
+    Snackbar.make(
+        view.context,
+        view.findViewById(id),
+        str,
+        Snackbar.LENGTH_SHORT
+    ).show()
+}
--- app/src/main/java/jr/brian/mybarber/view/activities/MainActivity.kt
+++ app/src/main/java/jr/brian/mybarber/view/activities/MainActivity.kt
@@ -1,15 +1,46 @@
 package jr.brian.mybarber.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import jr.brian.mybarber.R
 import jr.brian.mybarber.databinding.ActivityMainBinding
+import jr.brian.mybarber.model.util.replaceFragment
+import jr.brian.mybarber.view.auth_fragments.SignInFragment
+import jr.brian.mybarber.view.auth_fragments.SignUpFragment
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initListeners()
+    }
+
+    override fun onCheckedChanged(group: RadioGroup, checkId: Int) {
+        val checkRadioButton = group.findViewById<RadioButton>(group.checkedRadioButtonId)
+        checkRadioButton?.let {
+            when (checkRadioButton.id) {
+                R.id.addr_rb1 -> replaceFragment(R.id.container, SignInFragment())
+                else -> replaceFragment(R.id.container, SignUpFragment())
+            }
+        }
+    }
+
+    private fun initListeners() {
+        val group = findViewById<RadioGroup>(R.id.radio_group) as RadioGroup
+        group.setOnCheckedChangeListener(this)
+    }
+
+    override fun onBackPressed() {
+        val a = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_HOME)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(a)
     }
 }
