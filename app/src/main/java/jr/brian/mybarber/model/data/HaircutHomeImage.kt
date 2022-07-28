c1166bad442c8bd56a1fc7acb3df7c92ba2e7556
Brian Walker Jr <thaballa79@gmail.com>
2022-07-28 18:43:52 -0500

    adds placeholder images in HaircutHomeFragment

--- app/src/main/java/jr/brian/mybarber/model/data/HaircutHomeImage.kt
+++ app/src/main/java/jr/brian/mybarber/model/data/HaircutHomeImage.kt
@@ -2,4 +2,4 @@ package jr.brian.mybarber.model.data
 
 import androidx.annotation.DrawableRes
 
-data class HaircutHomeImage (@DrawableRes val image: Int)
+data class HaircutHomeImage (val image: String)
--- app/src/main/java/jr/brian/mybarber/view/fragments/HaircutHomeFragment.kt
+++ app/src/main/java/jr/brian/mybarber/view/fragments/HaircutHomeFragment.kt
@@ -29,7 +29,7 @@ class HaircutHomeFragment : Fragment() {
 
     private fun setAdapter() {
         initData()
-        homeImageAdapter = HaircutHomeImageAdapter(cuts)
+        homeImageAdapter = HaircutHomeImageAdapter(requireContext(), cuts)
         binding.apply {
             recyclerView.layoutManager = LinearLayoutManager(
                 requireContext(),
@@ -55,13 +55,48 @@ class HaircutHomeFragment : Fragment() {
     }
 
     private fun initData() {
-        cuts = ArrayList()
-        for (i in 1..7) {
-            cuts.add(
-                HaircutHomeImage(R.drawable.profile_pl_100)
+        cuts = ArrayList<HaircutHomeImage>().apply {
+            add(
+                HaircutHomeImage(
+                    "https://preview.redd.it/zvdm1l99tij81.jpg?" +
+                            "width=640&crop=smart&auto=webp&s=ac3d8f28164" +
+                            "7085cf246ccc3c2405658b800784a"
+                )
+            )
+            add(
+                HaircutHomeImage(
+                    "https://i.pinimg.com/564x/d3/25/cf/d325cf54b05983afa1e0a203471f4d94.jpg"
+                )
+            )
+            add(
+                HaircutHomeImage(
+                    "https://i.pinimg.com/564x/c6/d3/8f/c6d38f8835fa4cc46032bbb66c56b5b4.jpg"
+                )
+            )
+            add(
+                HaircutHomeImage(
+                    "https://therighthairstyles.com/wp-content/uploads/900x/designs-for-men/" +
+                            "1-mens-line-haircut-with-long-top.jpg"
+                )
+            )
+            add(
+                HaircutHomeImage(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9X6yxc" +
+                            "SkVT5_6Svy3K6mlPjo4786oXJzfYA&usqp=CAU"
+                )
+            )
+            add(
+                HaircutHomeImage(
+                    "https://www.menshairstyletrends.com/wp-content/uploads/2021/01" +
+                            "/High-fade-haircut-with-beard-nurii_b.jpg"
+                )
+            )
+            add(
+                HaircutHomeImage(
+                    "https://menshaircuts.com/wp-content/uploads/2021/12/how-to-bear" +
+                            "d-styles-imperial-short-hair-fade-683x1024.jpg"
+                )
             )
         }
     }
-
-
 }
